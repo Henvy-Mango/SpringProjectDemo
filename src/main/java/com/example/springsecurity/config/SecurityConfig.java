c5547e699afec9bc8f4be4198b64d5de6457b778
Naomi <mangogo.naomi@qq.com>
2022-01-12 14:54:16 +0800

    feat: 自定义拦截器
    补充注释
    添加关于页面

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.springsecurity.handler.MyAuthenticationFailureHandler;
 import com.example.springsecurity.handler.MyAuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,18 +25,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        // 表单登录
         http.formLogin()
+                // 用户密码 参数名配置
                 .usernameParameter("username")
                 .passwordParameter("password")
+
+                // 登录页面
                 .loginPage("/login.html")
+
+                // 登录接口地址
                 .loginProcessingUrl("/login")
+
+                // 登录成功处理器
                 // .successForwardUrl("/toMain")
                 .successHandler(new MyAuthenticationSuccessHandler("http://localhost:8080/main.html"))
+
+                // 登录失败处理器
                 // .failureForwardUrl("/toError");
                 .failureHandler(new MyAuthenticationFailureHandler("http://localhost:8080/error.html"));
 
         http.authorizeRequests()
+                // 全部放行
                 .antMatchers("/login.html", "/error.html").permitAll()
+
+                // 只允许匿名访问
+                .antMatchers("/about.html").anonymous()
+
+                // 放行文件夹
+                // .antMatchers("/js/**", "/css/**", "/image/**").permitAll()
+
+                // png图片放行
+                // .antMatchers("/**/*.png").permitAll()
+                .regexMatchers(HttpMethod.GET, ".+[.]png").permitAll()
+
+                // 其余拦截校验
                 .anyRequest().authenticated();
 
         http.csrf().disable();
